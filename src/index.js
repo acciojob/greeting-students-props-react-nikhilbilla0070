03c571bb293e4b3a30723d6bb136313da7369a6d
nikhilbilla0070 <114826561+nikhilbilla0070@users.noreply.github.com>
2023-02-14 23:40:35 +0530

    add solution

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
+import Welcome from "./components/Welcome";
 
 
 
+ReactDOM.render(<Welcome />, document.getElementById("root"));
 ReactDOM.render(<App />, document.getElementById("root"));
